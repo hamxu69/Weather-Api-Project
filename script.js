55ec49c6c6c68644f93c40dfc4204dd2331aa906
Hamza-Sama <hamxu69@gmail.com>
2026-07-06 01:47:06 +0500

    m

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 const button = document.querySelector(".day-btn");
 const searchInput = document.querySelector(".search-input");
 const dropdown = document.querySelector(".day-dropdown");
+const drop = document.querySelector(".dropdown");
 const searchDrop = document.querySelector(".search-drop");
 const selectedDay = document.querySelector(".selected-day");
-const unitDrop = document.querySelector(".units-menu");
+const unitDrop = document.querySelector(".units-btn");
 console.log(button, dropdown);
 
 unitDrop.addEventListener("click", (e) => {
   e.stopPropagation();
-  dropdown.classList.toggle("hidden");
+  drop.classList.toggle("hidden");
 });
-const days = document.querySelectorAll(".day-item");
-days.forEach((day) => {
-  day.addEventListener("click", function () {
-    days.forEach((el) => {
-      el.classList.remove("selected");
-    });
-    this.classList.add("selected");
-    selectedDay.textContent = this.textContent;
-    dropdown.classList.add("hidden");
-  });
-});
+
 button.addEventListener("click", (e) => {
   e.stopPropagation();
   dropdown.classList.toggle("hidden");
@@ -37,12 +28,10 @@ days.forEach((day) => {
   });
 });
 
-
 searchInput.addEventListener("click", (e) => {
   e.stopPropagation();
   searchDrop.classList.remove("hidden");
   console.log(searchDrop);
-  
 });
 const cities = document.querySelectorAll(".search-item");
 cities.forEach((city) => {
