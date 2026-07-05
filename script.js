7f244896ae7f0c11e31ccfc6c192ac78408a8a66
Hamza-Sama <hamxu69@gmail.com>
2026-07-06 00:31:12 +0500

    p

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const button = document.getElementByClassName("day-btn");
-console.log(button);
-button.addEventListener("click", function (e) {
-  console.log("clicked");
+const button = document.querySelector(".day-btn");
+const searchInput = document.querySelector(".search-input");
+const dropdown = document.querySelector(".day-dropdown");
+const searchDrop = document.querySelector(".search-drop");
+const selectedDay = document.querySelector(".selected-day");
+console.log(button, dropdown);
+
+button.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropdown.classList.toggle("hidden");
+});
+const days = document.querySelectorAll(".day-item");
+days.forEach((day) => {
+  day.addEventListener("click", function () {
+    days.forEach((el) => {
+      el.classList.remove("selected");
+    });
+    this.classList.add("selected");
+    selectedDay.textContent = this.textContent;
+    dropdown.classList.add("hidden");
+  });
+});
+
+
+searchInput.addEventListener("click", (e) => {
+  e.stopPropagation();
+  searchDrop.classList.remove("hidden");
+  console.log(searchDrop);
   
 });
-button.classlist.remove(".day-btn");
+const cities = document.querySelectorAll(".search-item");
+cities.forEach((city) => {
+  city.addEventListener("click", function () {
+    cities.forEach((el) => {
+      this.classList.add("selected");
+      searchInput.value = this.textContent;
+      searchDrop.classList.add("hidden");
+    });
+  });
+});
