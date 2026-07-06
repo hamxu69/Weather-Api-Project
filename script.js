4d65a213405599df56f01ce994d5b133c76cde35
Hamza-Sama <hamxu69@gmail.com>
2026-07-06 15:19:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ const cities = document.querySelectorAll(".search-item");
 cities.forEach((city) => {
   city.addEventListener("click", function () {
     cities.forEach((el) => {
-      this.classList.add("selected");
       searchInput.value = this.textContent;
       searchDrop.classList.add("hidden");
     });
+    this.classList.add("selected");
   });
 });
 
@@ -43,29 +43,24 @@ unitDrop.addEventListener("click", (e) => {
   e.stopPropagation();
   drop.classList.toggle("hidden");
 });
-// const tempList = document.querySelectorAll(".temp div");
-// tempList.forEach((tempOption) => {
-//   tempOption.addEventListener("click", function () {
-//     tempList.forEach((el) => {
-//       el.classList.remove("selected");
-//     });
-//     this.classList.add("selected");
-//   });
-// });
 const groups = document.querySelectorAll(".drop-group");
-
 groups.forEach((group) => {
   const items = group.querySelectorAll(".drop-item");
-
-  items.forEach((item) => {
-    item.addEventListener("click", function () {
+  console.log(items);
+  items.forEach((option) => {
+    option.addEventListener("click", function () {
+      console.log(option);
 
       items.forEach((el) => {
         el.classList.remove("selected");
       });
-
       this.classList.add("selected");
-
     });
   });
+});
+document.addEventListener("click", () => {
+
+  drop.classList.add("hidden");
+  searchDrop.classList.add("hidden");
+  dropdown.classList.add("hidden");
 });
