1de51b6b3044e1d0816963dd003d9edcfc05af3d
Hamza-Sama <184765708+hamxu69@users.noreply.github.com>
2026-07-12 00:41:19 +0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import NewApp from "./components/NewApp";
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <>
+      <h1>My First React App</h1>
+
+      <NewApp />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NewApp.tsx
@@ -0,0 +1,4 @@
+function NewApp() {
+  return <input type="text" placeholder="enter text here..."/>
+}
+export default NewApp;
